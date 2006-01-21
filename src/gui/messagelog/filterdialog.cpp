6b3302262cb3e8aa801cff1fb656956cdb0fb242
Justin Hipple <hipplej@vidalia-project.net>
2006-01-21 04:34:19 +0000

    Tooltips are now sane. FilterDialog is now smaller and denser.
    svn:r146

--- src/gui/messagelog/filterdialog.cpp
+++ src/gui/messagelog/filterdialog.cpp
@@ -9,6 +9,21 @@ FilterDialog::FilterDialog(VidaliaSettings *settings, QWidget *parent)
   /* Save the VidaliaSettings object for later */
   _settings = settings;
 
+  /* Set the tooltips for the checkboxes */
+  ui.chkTorErr->setToolTip(tr("Messages that appear when something has \ngone very wrong and Tor cannot proceed."));
+  
+  ui.chkTorWarn->setToolTip(tr("Messages that only appear when \nsomething has gone wrong."));
+
+  ui.chkTorNote->setToolTip(tr("Messages that appear infrequently \nduring normal Tor operation and are \nnot considered errors, but you may \ncare about."));
+
+  ui.chkTorInfo->setToolTip(tr("Messages that appear frequently \nduring normal Tor operation."));
+
+  ui.chkTorDebug->setToolTip(tr("Hyper-verbose messages primarily of \ninterest to Tor developers.")); 
+
+  ui.chkVidErr->setToolTip(tr("Messages that appear when something \nhas gone very wrong with Vidalia.")); 
+
+  ui.chkVidStat->setToolTip(tr("Messages that appear freqently indicating \nVidalia operation status information."));
+
   /* Set the checkboxes on the form accordingly */
   ui.chkTorErr->setChecked(_settings->getShowMsg(MSG_TORERR));
   ui.chkTorWarn->setChecked(_settings->getShowMsg(MSG_TORWARN));
