81ff12ae0e001d8dd6b0f4cb12ce813aeb797bd5
Matt Edman <edmanm@vidalia-project.net>
2006-02-08 07:06:31 +0000

    Following in Rev 244's footsteps, we no longer need to say "Tor Error" and such. "Error" is just fine.
    svn:r248

--- src/config/messagetypes.h
+++ src/config/messagetypes.h
@@ -1,10 +1,10 @@
 
 /* Message Types */
-#define MSG_TOR_ERROR  "Tor Error"
-#define MSG_TOR_WARN   "Tor Warning"
-#define MSG_TOR_NOTICE "Tor Notice"
-#define MSG_TOR_INFO   "Tor Info"
-#define MSG_TOR_DEBUG  "Tor Debug"
+#define MSG_TOR_ERROR  "Error"
+#define MSG_TOR_WARN   "Warning"
+#define MSG_TOR_NOTICE "Notice"
+#define MSG_TOR_INFO   "Info"
+#define MSG_TOR_DEBUG  "Debug"
 
 #define TOR_ERROR      (1u<<0)
 #define TOR_WARN       (1u<<1)
