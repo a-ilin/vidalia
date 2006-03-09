b820817844bfe57181a4bab9746355c281d35e01
Matt Edman <edmanm@vidalia-project.net>
2006-03-09 21:09:31 +0000

    Move makedist.sh to pkg/ and factor the OSX dmg stuff out into its own script in pkg/osx/ that gets called from makedist.sh
    svn:r414

---
+++ pkg/makedist.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Builds distribution packages for various platforms.
+
+
+# Check for proper script arguments
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: $0 <tarball|win32|osx>"
+  exit 1
+fi
+
+
+# Make the distribution depending on what type was requested
+case "$1" in
+"tarball")
+  # Put the tarball in the current directory
+  destdir=`pwd`
+  srcdir=`dirname $(pwd) | sed -e "s/.*\///"`
+  tarball="$destdir/$srcdir.tar.gz"
+  
+  echo "Creating source tarball: $tarball"  
+  pushd "../.." 1>/dev/null
+  svn ls -R $srcdir/ | 
+     sed -e "s/^/$srcdir\/&/" | 
+     sed -e "/\/$/d" | 
+     tar -cz -T - -f "$tarball"
+  popd -1 1>/dev/null
+  ;;
+  
+"osx")
+  dmg=`dirname $(pwd) | sed -e "s/.*\///"`.dmg
+  srcdir="../"
+  srcfiles="bin README AUTHORS LICENSE CHANGELOG"
+  osx/builddmg.sh "$srcdir" "$srcfiles" "$dmg"
+  ;;
+
+"win32")
+  echo "Unimplemented"
+  ;;
+
+*)
+  echo "Unrecognized distribution type."
+  ;;
+esac
+
+exit $?
+
---
+++ pkg/osx/builddmg.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+
+# Make sure enough arguments were supplied
+if [ "$#" -ne 3 ]
+then
+  echo "Usage: $0 <srcdir> <files to include> <outfile.dmg>"
+  exit 1
+fi
+
+# Make sure the source directory exists
+srcdir=$1
+if [ ! -d "$srcdir" ]
+then
+  echo "Specified source directory doesn't exit: $srcdir"
+  exit 1
+fi
+echo "Source Directory: $srcdir"
+
+# Get the rest of the command-line arguments
+filelist=$2
+echo "Files to copy: $filelist"
+dmg=$3
+echo "Destination Disk Image: $dmg"
+volname=`echo "$dmg" | sed -e "s/.*\///" | sed -e "s/.dmg//"`
+echo "Volume Name: $volname"
+
+# Create the disk image
+hdiutil create $dmg -size 13m -fs HFS+ -volname "$volname"
+
+# Mount the image and get the device name
+mntdir="dist.mnt"
+hdiutil attach "$dmg" -noautoopen -quiet -mountpoint "$mntdir"
+dev_handle=`hdiutil info | grep "$mntdir" | grep "Apple_HFS" | awk '{print $1}'`
+
+# Copy over the specified files into the disk image
+for file in $filelist
+do
+  echo "Copying $srcdir/$file to $mntdir/"
+  ditto -rsrc "$srcdir/$file" "$mntdir/"
+done
+
+# Unmount the volumen
+hdiutil detach $dev_handle
+
+# Replace the image with a compressed image
+hdiutil convert $dmg -format UDZO -o $dmg.udzo.dmg
+rm -f $dmg
+mv $dmg.udzo.dmg $dmg
+
