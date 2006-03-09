1ab0172ab007c08cd98d26a3afc0d5f6adb8f069
Matt Edman <edmanm@vidalia-project.net>
2006-03-09 18:16:27 +0000

    Add support for building the OS X dmg to makedist.sh. This should probably be factored out into a script in the /pkg/ directory, but it lives here for now.
    svn:r412

--- makedist.sh
+++ makedist.sh
@@ -10,21 +10,34 @@ then
   exit 1
 fi
 
+srcdir=`pwd | sed -e "s/.*\///"`
+
 # Make the distribution depending on what type was requested
 case "$1" in
 "tarball")
   echo "Creating source tarball..."
-  srcdir=`pwd | sed -e "s/.*\///"`
   pushd ".." 1>/dev/null
-  `svn ls -R "$srcdir"/ | 
+  svn ls -R $srcdir/ | 
      sed -e "s/^/$srcdir\/&/" | 
      sed -e "/\/$/d" | 
-     tar -cz -T - -f "$srcdir".tar.gz`
+     tar -cz -T - -f $srcdir.tar.gz
   popd 1>/dev/null
   ;;
   
 "osx")
-  echo "Unimplemented"
+  dmg=$srcdir.dmg
+  echo "Creating OS X Disk Image: $dmg"
+  mntdir="dist.mnt"
+  pushd ".." 1>/dev/null
+  hdiutil create $dmg -size 13m -fs HFS+ -volname "$srcdir"
+  hdiutil attach "$dmg" -noautoopen -quiet -mountpoint "$mntdir"
+  dev_handle=`hdiutil info | grep "$mntdir" | grep "Apple_HFS" | awk '{print $1}'`
+  ditto -rsrc $srcdir/bin $mntdir/
+  hdiutil detach $dev_handle
+  hdiutil convert $dmg -format UDZO -o $srcdir.udzo.dmg
+  rm -f $dmg
+  mv $srcdir.udzo.dmg $dmg
+  popd 1>/dev/null
   ;;
 
 "win32")
