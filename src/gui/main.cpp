a0149c8d7fbf3322f363be0c2ec5224c68a94bac
Matt Edman <edmanm@vidalia-project.net>
2006-03-24 04:55:12 +0000

    Handle command-line arguments a little better and give Vidalia a usage message.
    svn:r471

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -29,8 +29,9 @@
  */
  
 #include <QApplication>
+#include <QTextStream>
 
-#ifdef Q_WS_MAC
+#if defined(Q_WS_MAC)
 #include <QMacStyle>
 #else
 #include <QPlastiqueStyle>
@@ -39,39 +40,91 @@
 #include <config/vidaliasettings.h>
 #include "mainwindow.h"
 
+
+/** Prints the available arguments for Vidalia. The format of the usage
+ * message is:
+ *
+ * Usage:
+ *   vidalia [options]
+ *
+ * Available Options:
+ *   -help          Displays this usage message and exits.
+ *   -reset         Resets ALL stored Vidalia settings.
+ */
 void
-process_arguments()
+print_usage()
 {
-  QStringList args = QCoreApplication::arguments();
-  if (args.contains("-reset")) {
-    VidaliaSettings settings;
-    settings.reset();
+  QTextStream out(stdout);
+  out << "Usage: " << endl;
+  out << "\t" << QCoreApplication::arguments().at(0) << " [options]" << endl;
+  out << endl << "Available Options:" << endl;
+  out << "\t-help\t\tDisplays this usage message and exits." << endl;
+  out << "\t-reset\t\tResets ALL stored Vidalia settings." << endl;
+  out << endl;
+}
+
+/** A simple wrapper that handles stripping a leading "-" or "--", if given,
+ * on a command-line argument and converting the argument to all-lowercase. */
+QString
+get_argument_name(QString arg)
+{
+  if (arg.startsWith("-")) {
+    arg = arg.mid((arg.startsWith("--") ? 2 : 1));
   }
+  return arg.toLower();
+}
+
+/** Parses the arguments specified on the command-line and handles arguments
+ * that are followed by values. */
+bool
+parse_arguments(QMap<QString, QString> &argMap)
+{
+  QStringList argv = QCoreApplication::arguments();
+  
+  QString arg;
+  for (int i = 1; i < argv.size(); i++) {
+    arg = get_argument_name(argv.at(i));
+    
+    /* Handle arguments with values here */ 
+
+    argMap.insert(arg, "");
+  }
+  return true;
+}
+
+/** Applies an appropriate GUI style. If we're building on Mac, 
+ * then use the Mac style, otherwise we'll use the Plastique style. */
+void
+apply_gui_style()
+{
+#if defined(Q_WS_MAC)
+  QApplication::setStyle(new QMacStyle);
+#else
+  QApplication::setStyle(new QPlastiqueStyle);
+#endif
 }
 
 /** Main application entry point. */
 int 
 main(int argc, char *argv[])
 {
-  
   Q_INIT_RESOURCE(vidalia);
 
+  /* Parse and validate any command-line arguments */
   QApplication app(argc, argv);
-  
-  /* Handle any command-line arguments */
-  if (argc > 1) {
-    process_arguments();
+  QMap<QString,QString> argMap;
+  if (!parse_arguments(argMap) || argMap.contains("help")) {
+    print_usage();
+    return -1;
   }
-
-/* 
-  If we're building on Mac, then use the Mac style,
-  otherwise we'll use the Plastique style.
-*/
-#ifdef Q_WS_MAC
-  QApplication::setStyle(new QMacStyle);
-#else
-  QApplication::setStyle(new QPlastiqueStyle);
-#endif
+   
+  /* Check if we're supposed to reset our command-line arguments */
+  if (argMap.contains("reset")) {
+    VidaliaSettings::reset();
+  }
+  
+  /* Apply a gui style, depending on OS */
+  apply_gui_style();
 
   /* Since we don't have a visible main window, if we were to display a
    * QMessageBox (for example, to display an error when starting or stopping
