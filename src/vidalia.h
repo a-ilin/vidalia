310f7eddde8c419fb5ff55e8241cb9f656c9c8fa
Matt Edman <edmanm@vidalia-project.net>
2006-06-23 02:59:40 +0000

    Bump to 0.0.6 and give the changelog a real date.
    svn:r1050

--- src/vidalia.h
+++ src/vidalia.h
@@ -41,7 +41,7 @@
 #include <control/torcontrol.h>
 
 /** Vidalia's version string */
-#define VIDALIA_VERSION    "0.0.6-svn"
+#define VIDALIA_VERSION    "0.0.6"
 
 
 class Vidalia : public QApplication
