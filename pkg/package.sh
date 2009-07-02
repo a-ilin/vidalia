d6df0f40d2a2f9f1e1289dc0be637abba0a23f08
Matt Edman <edmanm@vidalia-project.net>
2009-07-02 00:32:59 +0000

    Remove the "osx-bundle" and "osx-split-bundle" options from package.sh since those have been moved into proper CMake build targets. Also remove builddmg.sh.in since nothing uses it anymore.
    svn:r3933

--- pkg/package.sh
+++ pkg/package.sh
@@ -16,76 +16,12 @@
 # Check for proper script arguments
 if [ "$#" -eq 0 ]
 then
-  echo "Usage: $0 <osx-bundle|osx-split-bundle|rpm>"
+  echo "Usage: $0 <rpm>"
   exit 1
 fi
 
 # Make the distribution depending on what type was requested
 case "$1" in
-#
-# OS X (Bundle)
-#
-"osx-bundle")
-  if [ $# -ne 4 ]
-  then
-    echo "Usage: $0 osx-bundle <path-to-tor> <path-to-polipo> <path-to-torbutton-file>"
-    exit 1
-  fi
-  torpath="$2"
-  polipopath="$3"
-  torbuttonpath="$4"
-  torversion=`echo "$torpath" | sed -e "s/.*\///" | sed -e "s/tor-//"`
-  
-  mkdir ../LEGAL
-  mkdir ../.background
-  cp -R ../README ../src/vidalia/Vidalia.app ../
-  cp osx/background.png ../.background/
-  cp osx/bundle-ds_store ../.DS_Store
-  cp osx/vidalia.conf.sample ../Vidalia.app/Contents/Resources/vidalia.conf
-  cp ../CREDITS ../CHANGELOG ../Vidalia.app/
-  cp ../LICENSE ../LICENSE-GPLV2 ../LICENSE-GPLV3 ../LICENSE-LGPLV3 ../LICENSE-OPENSSL ../LEGAL/
-  cp -R ../LEGAL ../Vidalia.app/
-  cp $torpath/src/or/tor $torpath/src/tools/tor-checkkey $torpath/src/tools/tor-gencert $torpath/src/tools/tor-resolve ../Vidalia.app/
-  cp $torpath/src/config/geoip $torpath/src/config/torrc.sample ../Vidalia.app/
-  cp $polipopath/polipo $polipopath/contrib/tor-polipo.conf ../Vidalia.app/
-  cp $torbuttonpath ../Vidalia.app/
-  srcdir="../"
-  srcfiles="Vidalia.app README LEGAL .background .DS_Store"
-  osx/builddmg.sh "$srcdir" "$srcfiles" "bundle" "$torversion"
-  ;;
-
-#
-# OS X (Split Bundle)
-#
-"osx-split-bundle")
-  if [ $# -ne 4 ]
-  then
-    echo "Usage: $0 osx-split-bundle <path-to-tor> <path-to-polipo> <path-to-torbutton-file>"
-    exit 1
-  fi
-  torpath="$2"
-  polipopath="$3"
-  torbuttonpath="$4"
-  torversion=`echo "$torpath" | sed -e "s/.*\///" | sed -e "s/tor-//"`
-  
-  mkdir ../LEGAL
-  mkdir ../.background
-  cp -R ../README ../src/vidalia/Vidalia.app ../
-  cp osx/background.png ../.background/
-  cp osx/bundle-ds_store ../.DS_Store
-  cp osx/vidalia.conf.sample ../Vidalia.app/Contents/Resources/vidalia.conf
-  cp ../CREDITS ../CHANGELOG ../Vidalia.app/
-  cp ../LICENSE ../LICENSE-GPLV2 ../LICENSE-GPLV3 ../LICENSE-LGPLV3 ../LICENSE-OPENSSL ../LEGAL/
-  cp -R ../LEGAL ../Vidalia.app/
-  cp $torpath/src/or/tor $torpath/src/tools/tor-checkkey $torpath/src/tools/tor-gencert $torpath/src/tools/tor-resolve ../Vidalia.app/
-  cp $torpath/src/config/geoip $torpath/src/config/torrc.sample ../Vidalia.app/
-  cp $polipopath/polipo $polipopath/contrib/tor-polipo.conf ../Vidalia.app/
-  cp $torbuttonpath ../Vidalia.app/
-  srcdir="../"
-  srcfiles="Vidalia.app README LEGAL .background .DS_Store"
-  osx/builddmg.sh "$srcdir" "$srcfiles" "split-bundle" "$torversion"
-  ;;
-  
 #
 #  rpm package
 #
