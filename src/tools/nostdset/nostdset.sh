90f3dffd126356e0a6ce0bae1c7e576b89873edd
Tomas Touceda <chiiph@gentoo.org>
2010-11-04 17:13:15 +0000

    Adds nostdset script to remove stdset property from UI files
    svn:r4484

---
+++ src/tools/nostdset/nostdset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+## $Id$
+## 
+##  This file is part of Vidalia, and is subject to the license terms in the
+##  LICENSE file, found in the top level directory of this distribution. If 
+##  you did not receive the LICENSE file with this file, you may obtain it
+##  from the Vidalia source package distributed by the Vidalia Project at
+##  http://www.vidalia-project.net/. No part of Vidalia, including this file,
+##  may be copied, modified, propagated, or distributed except according to
+##  the terms described in the LICENSE file.
+##
+
+find -name *.ui | xargs sed "s/stdset=\"0\" //" -i
