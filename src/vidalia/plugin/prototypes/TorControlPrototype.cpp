fe83b46396a9f069710f64a5e0cfd2b13406a92b
Feroze Naina <ferozenaina@gmail.com>
2012-05-29 23:41:05 +0530

    Move macros to external file

--- src/vidalia/plugin/prototypes/TorControlPrototype.cpp
+++ src/vidalia/plugin/prototypes/TorControlPrototype.cpp
@@ -14,56 +14,7 @@
 */
 
 #include "TorControlPrototype.h"
-
-#define GET_AND_CALL(type, func, res) \
-  type obj = qscriptvalue_cast<type>(thisObject()); \
-  if(obj) \
-    res = obj->func;
-
-#define MERGE2(result, errmsg) \
-  QVariant(QList<QVariant>() << result << errmsg);
-
-#define DEF_TYPE0(type, retType, func, call) \
-retType \
-type##Prototype::func \
-{ \
-  type *obj = qscriptvalue_cast<type *>(thisObject()); \
-  if(obj) \
-    return obj->call; \
-}
-
-#define DEF_TYPE1(type, resType, func, call) \
-QVariant \
-type##Prototype::func \
-{ \
-  resType res; \
-  QString errmsg; \
-\
-  type *obj = qscriptvalue_cast<type *>(thisObject()); \
-  if(obj) \
-    res = obj->call; \
-  QList<QVariant> vals; \
-  vals << res << QVariant(errmsg); \
-\
-  return vals; \
-}
-
-#define DEF_TYPE2(type, resType, ansType, func, call) \
-QVariant \
-type##Prototype::func \
-{ \
-  resType res; \
-  ansType ans; \
-  QString errmsg; \
-\
-  type *obj = qscriptvalue_cast<type *>(thisObject()); \
-  if(obj) \
-    res = obj->call; \
-  QList<QVariant> vals; \
-  vals << QVariant(ans) << res << QVariant(errmsg); \
-\
-  return vals; \
-}
+#include "prototypemacros.h"
 
 TorControlPrototype::TorControlPrototype()
   : QObject(), QScriptable() {}
